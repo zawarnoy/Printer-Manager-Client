55efd00204bbe79811ef21f7f9d6ef7275f196b9
Vlad <vladdemidik@gmail.com>
2019-05-24 02:22:20 +0300

    s

--- app/Services/Server/ServerCommunicationService.php
+++ app/Services/Server/ServerCommunicationService.php
@@ -7,7 +7,10 @@ namespace App\Services\Server;
 class ServerCommunicationService
 {
 
+    // Будем использовать этот мод для получения инфы о принтерах
     const MODE_GET_INFO = 1;
+
+    // Непосредственно для печати файлов
     const MODE_PRINT_FILE = 2;
 
     public function __construct()
